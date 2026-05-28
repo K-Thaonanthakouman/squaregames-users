d69577b1136a2e726a6722ccaa6316684581cbea
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-05-28 14:35:44 +0200

    Initialisation projet squaregames-users

---
+++ src/test/java/com/squaregames/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squaregames.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
